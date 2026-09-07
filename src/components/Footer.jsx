0ef820847609029d1d37567e81645e10f4ec49be
dipaawhii <widyasaridiva@gmail.com>
2026-09-07 11:48:40 +0700

    Hapus Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
    <footer
         className="py-8 text-center border-t border-slate-200 dark:border-slate-800 bg-white/50 dark:bg-slate-900/50 backdrop-blur-md">
         <div className="container mx-auto px-6">
-            <p className="text-slate-600 dark:text-slate-400 font-medium text-sm flex justify-center items-center gap-1">
-                &copy; <span>2026</span>Clara. Built with<FaHeart className=" text-primary"/>   using TailwindCSS & Vanilla JS.
-            </p>
+            {/* <p className="text-slate-600 dark:text-slate-400 font-medium text-sm flex justify-center items-center gap-1">
+                &copy; <span>2026</span>Dipaw. Built with<FaHeart className=" text-primary"/>   using TailwindCSS & Vanilla JS.
+            </p> */}
         </div>
     </footer>
   )
